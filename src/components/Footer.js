2596cdfb38930d509e0eecdb9e78b4360150e4a8
vamsitadiparthi <tadiparthivamsi@gmail.com>
2024-07-23 16:30:39 +0530

    my portfolio

---
+++ src/components/Footer.js
@@ -0,0 +1,31 @@
+import { Container, Row, Col } from "react-bootstrap";
+// import { MailchimpForm } from "./MailchimpForm";
+import logo from "../assets/img/logo.svg";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
+import navIcon4 from '../assets/img/nav-icon10.svg';
+import navIcon5 from '../assets/img/leetcode.svg';
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          {/* <MailchimpForm /> */}
+          <Col size={12} sm={6}>
+            <img src={logo} alt="Logo" />
+          </Col>
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+              <a href="https://www.linkedin.com/in/vamsi-tadiparthi-420070225/"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://github.com/vamsi190146"><img src={navIcon4} alt="Icon" /></a>
+              <a href="https://leetcode.com/u/tadiparthivamsi/"><img src={navIcon5} alt="Icon" /></a>
+            </div>
+            <p>Owner:Tadiparthi Vamsi,Copyright 2022. All Rights Reserved</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
